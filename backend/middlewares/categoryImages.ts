e536c44f5abc968c3bd22594e431d001f79d038e
CLEMON EZEH <clemonezeh@gmail.com>
2024-02-22 11:11:53 +0100

    finished and tested categories api

--- backend/middlewares/categoryImages.ts
+++ backend/middlewares/categoryImages.ts
@@ -2,11 +2,11 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads/products");
+    cb(null, "./uploads/category");
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
   },
 });
 
-export const upload = multer({ storage: storage });
+export const categoryUpload = multer({ storage: storage });
--- backend/routes/categoryRoutes.ts
+++ backend/routes/categoryRoutes.ts
@@ -6,13 +6,17 @@ import {
   createCategory,
   addSubCategory,
 } from "../controllers/categories";
-
+import { categoryUpload } from "../middlewares/categoryImages";
 const router = Router();
 
 router.get("/v1/all", getAllCategories);
 router.get("/v1/product", getProductCategories);
 router.get("/v1/:id", getSingleCategory);
-router.post("/v1/create", createCategory);
-router.patch("/v1/:categoryId", addSubCategory);
+router.post("/v1/create", categoryUpload.single("catImage"), createCategory);
+router.patch(
+  "/v1/:categoryId",
+  categoryUpload.single("subImage"),
+  addSubCategory,
+);
 
 export default router;
