99b48f49e4d5b01b4ec7c398b625ce95078add53
CLEMON EZEH <clemonezeh@gmail.com>
2024-02-22 11:40:34 +0100

    finished change password feature

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import users from "../models/userModel";
+import bcrypt from "bcrypt";
 
 // checking to see which user is logged in
 export const getUser = async (req: Request, res: Response) => {
@@ -35,4 +36,33 @@ export const updateUserInfo = async (req: Request, res: Response) => {
   } catch (err: any) {
     res.status(500).json(err.message);
   }
+};
+
+// Change user password
+export const changeUserPassword = async (req: Request, res: Response) => {
+  try {
+    const { oldPassword, newPassword } = req.body;
+    const userId = req.user;
+    const currentUser = await users.findById(userId);
+    // check if current user exists
+    if (!currentUser) {
+      res.status(401).json("User not found");
+    }
+    const validateOldPassword = bcrypt.compare(
+      oldPassword,
+      //@ts-ignore
+      currentUser?.password,
+    );
+    // check if the old password is correct
+    if (!validateOldPassword) {
+      res.status(401).json("Invalid old password");
+    }
+    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+
+    const updatePassword = await users.findByIdAndUpdate(userId, {
+      password: hashedNewPassword,
+    });
+
+    res.status(200).json("Password updated successfully");
+  } catch (err: any) {}
 };
--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { getUser } from "../controllers/userController";
+import {
+  changeUserPassword,
+  getUser,
+  updateUserInfo,
+} from "../controllers/userController";
 import { isBoth, verifyToken } from "../middlewares/JWT";
 
 const router = Router();
 
 router.get("/v1/users", verifyToken, isBoth, getUser);
+router.patch("/v1/users/updateInfo", verifyToken, updateUserInfo);
+router.patch("/v1/users/changePassword", verifyToken, changeUserPassword);
 
 export default router;
